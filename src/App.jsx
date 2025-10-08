9a1e7938c72fd6b9abd8df5c82c0d5169a83cea2
Murilo Neto <37521@raphaeldisanto.com.br>
2025-10-08 12:17:19 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { CloudSun } from 'lucide-react';
+import { CloudSun, MapPin } from 'lucide-react';
 
 function App() {
   const [cidade, setCidade] = useState('');
@@ -38,15 +38,41 @@ function App() {
             </h1>
           <p>Exemplo de consumo de API com React</p>
         </header>
+
+        {/* Caixa de Busca */}
         <div className="busca-box">
           <div className="busca-container">
             <input
              type="text" 
-             placeholder="Digite o nome da cidade.."/>
+             placeholder="Digite o nome da cidade.."
+             />
             <button>Buscar</button>
             </div>
-            
-        </div>
+          </div>
+
+            {/* Resultado do Clima */}
+            <div id="card-resultado">
+              <div id="cidade-info">
+                <div id="cidade-nome">
+                <MapPin style={{color: '#3562eb'}} size={48} />
+                  Campinas, BR
+                </div>
+                <p id="cidade-desc">
+                  Nublado
+                </p>
+                </div>{/* </div>Fecha #cidade-desc */}
+
+                {/* Temperatura principal */}
+                <div id="temperatura-box">
+                  <div id="temp-valor">21°C</div>
+                  <div id="temp-sens"></div>
+                    Sensação Térmica: 21°C
+                  </div>
+                
+
+            </div>{/* Fecha #card-resultado*/}
+
+        
       </div>
      </div>
     </>
